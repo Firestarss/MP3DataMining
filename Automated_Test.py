1ee1789b6a6fa42f5fbcce2a965f2394abd4f462
Florian Schwarzinger <Firestarss88@gmail.com>
2019-10-15 12:01:17 -0400

    added automated testing to check how effective the program is

---
+++ Automated_Test.py
@@ -0,0 +1,133 @@
+import json
+import math
+
+#open necessary files
+with open("champion.json") as file:
+    champion_data = json.load(file)
+with open("FullPlayerChampAndWin.txt") as file:
+    players = file.readlines()
+
+#declare variables that will be needed later
+champion_picks = dict()
+champion_wins = dict()
+champion_win_rate = dict()
+blue_red_advantage = [0,0]
+
+#populate the dictionaries to make adding to them later easier
+for value in champion_data.keys():
+    champion_picks[int(value)] = 0
+    champion_wins[int(value)] = 0
+    champion_win_rate[int(value)] = 50
+
+def idToChampion(id_number):
+    """
+    takes the id number of a champion and returns it's name
+    """
+    return champion_data[str(id_number)]
+
+def getBlueTeam(match):
+    """
+    returns the champions on the blue team in a specific match
+    """
+    return(match[0][0])
+
+def getRedTeam(match):
+    """
+    returns the champions on the red team in a specific match
+    """
+    return(match[0][1])
+
+def getWinner(match):
+    """
+    returns the winner of a sepcific match
+    """
+    return(match[1])
+
+def getBlueWinRate(team_wins):
+    """
+    Finds the percentage of times that the blue team won
+    """
+    return team_wins[0]/(team_wins[1]+team_wins[0])
+
+def getRedWinRate(team_wins):
+    """
+    Finds the percentage of times that the red team won
+    """
+    return team_wins[1]/(team_wins[1]+team_wins[0])
+
+def combineBothWinRates(blue_input, red_input):
+    blue_win_chance = getCombinedWinrate(blue_input, "blue", blue_red_advantage)
+    red_win_chance = getCombinedWinrate(red_input, "red", blue_red_advantage)
+    return blue_win_chance,red_win_chance
+
+def getCombinedWinrate(champions, team_color, team_wins):
+    """
+    Combines the winrates of all the champions on a team with the winrate
+    of the side the team is on
+    """
+    out = 0
+    for champion in champions:
+        out += champion_win_rate[champion]
+    if team_color == "blue":
+        out += getBlueWinRate(team_wins)
+    if team_color == "red":
+        out += getRedWinRate(team_wins)
+
+    return out
+
+#calculates the win rates of all the champions from the given matches
+for matches in players:
+    matches = eval(matches)
+    for match in matches:
+
+        blue_team = getBlueTeam(match)
+        red_team = getRedTeam(match)
+        winner = getWinner(match)
+
+        #increments champion_wins for all the champions on the winning team
+        if winner == "None":
+            continue
+
+        if winner == "Blue":
+            blue_red_advantage[0] += 1
+            for value in blue_team:
+                champion_wins[value] += 1
+
+        if winner == "Red":
+            blue_red_advantage[1] += 1
+            for value in red_team:
+                champion_wins[value] += 1
+
+        #increments champion_picks of all champions that were picked for the match
+        for value in red_team:
+            champion_picks[value] += 1
+
+        for value in blue_team:
+            champion_picks[value] += 1
+
+#turns champion_picks and champion_wins into actual win percentages
+for key in champion_win_rate:
+    if champion_picks[key] > 0 and champion_wins[key] > 0:
+        champion_win_rate[key] = (champion_wins[key]/champion_picks[key])*100
+
+
+correct_count = 0
+match_count = 0
+for matches in players:
+    matches = eval(matches)
+    for match in matches:
+
+        blue_team = getBlueTeam(match)
+        red_team = getRedTeam(match)
+        winner = getWinner(match)
+
+        blue_win_chance, red_win_chance = combineBothWinRates(blue_team, red_team)
+        match_count += 1
+
+        if blue_win_chance > red_win_chance and winner == "Blue":
+            correct_count += 1
+
+        if blue_win_chance > red_win_chance and winner == "Red":
+            correct_count += 1
+
+print(correct_count/match_count*100)
